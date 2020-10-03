bbedf5f0524282bdb3cd26866e5ebbb13a50c8da
Savage-E <Savage-E@users.noreply.github.com>
2020-10-03 19:47:20 +0300

    Fitst commit

---
+++ src/main/java/ru/mail/guice/Main.java
@@ -0,0 +1,12 @@
+package ru.mail.guice;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.jetbrains.annotations.NotNull;
+
+public class Main {
+    public static void main(@NotNull String[] args) {
+        final Injector injector = Guice.createInjector(new MainModule());
+         injector.getInstance(LogHandler.class).waitForInput();
+    }
+}
